d5f76f84f67e7ed4539e2b62db994f9a73544c9b
Neloy Ahmed <some@yahoo.com>
2016-08-22 20:26:33 +0600

    Initial commit after writing heredoc_variableFuncion

---
+++ heredoc_variableFunction.php
@@ -0,0 +1,13 @@
+<?php
+    
+   function neloy(){
+      echo  <<<Nel
+           this is neloy a "COMPUTER PROGRAMMER"<br/>
+            Neloy loves to make algorithm to solve complex<br/>
+           programming problem
+Nel;
+   }
+
+$something = "neloy";    
+$something(); //Variable function example
+?>
